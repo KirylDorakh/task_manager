a494907feed56912e6e31665228a733f16c3d210
Kiryl Dorakh <kirill.dorokh@gmail.com>
2025-03-12 20:36:20 +0400

    login page

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, session, render_template, redirect
+from flask import Flask, session, render_template, redirect, request
 from flask_session import Session
 from dotenv import load_dotenv
 
@@ -30,5 +30,43 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        # Check the data from form
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        # # Ensure username was submitted
+        # if not request.form.get("username"):
+        #     return apology("must provide username", 403)
+
+        # # Ensure password was submitted
+        # elif not request.form.get("password"):
+        #     return apology("must provide password", 403)
+
+        # Query database for username
+        user = User.query.filter_by(username=username).first()
+
+        print(user, user.check_password(password))
+
+        # Remember which user has logged in
+        # session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+

299100e957a34a4bec71b7b6273ff8b247e353aa
Kiryl Dorakh <kirill.dorokh@gmail.com>
2025-03-15 11:37:02 +0400

    Change password logic and page

--- run.py
+++ run.py
@@ -141,11 +141,80 @@ def register():
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("register.html")
-    
 
-@app.route("/test")
-def test():  
-    return render_template("test.html") 
+
+@app.route("/change_password", methods=["GET", "POST"])
+def change_password():
+
+    # If method POST
+    if request.method == "POST":
+
+        # If user is not log in
+        if not session.get("username"):
+            username = request.form.get("username")
+
+            # Ensure username was submitted
+            if not username:
+                return render_template("change_password.html",  apology="Please, write an username")
+
+            # Query database for user's information
+            user = User.query.filter_by(username=username).first()
+
+            # Ensure username exists
+            if not user:
+                return render_template("change_password.html",  apology="Please, write a correct username")
+        
+        else:
+            user = User.query.filter_by(username=session["username"]).first()
+
+            # Ensure username exists
+            if not user:
+                return render_template("change_password.html",  apology="Invalid user")
+
+        old_password = request.form.get("old_password")
+        new_password = request.form.get("new_password")
+        confirmation = request.form.get("confirmation")
+
+        # Ensure old password was submitted
+        if not old_password:
+            return render_template("change_password.html",  apology="must provide old password")
+
+        # Ensure old password is correct
+        elif not user.check_password(old_password):
+            return render_template("change_password.html",  apology="invalid old password")
+
+        # Ensure mew password was submitted
+        elif not new_password:
+            return render_template("change_password.html",  apology="must provide new password")
+
+        # Ensure confirmation was submitted
+        elif not confirmation:
+            return render_template("change_password.html",  apology="must provide confirmation of new password")
+
+        # Ensure new password and confirmation match
+        elif new_password != confirmation:
+            return render_template("change_password.html",  apology="new password and new password confirmation don't match")
+
+
+        # Change password and commit the update
+        try:
+            user.set_password(new_password)
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            return render_template("change_password.html",  apology="DB error: " + str(e))
+        else:
+            # Logout user
+            session.clear()
+            # Redirect to login page
+            return render_template("login.html", success="Password was changed! Please log in with your new password")
+
+    # If method GET and user is logged in
+    elif session.get("username"):
+        return render_template("change_password.html")
+
+    # If method GET and user is not logged in
+    return render_template("change_password.html")
 
 
 if __name__ == '__main__':
